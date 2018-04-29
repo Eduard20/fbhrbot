ecbdd836f96bbee781e0ed942f425aaef8a2751c
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2018-04-29 22:06:49 +0400

    added morgan

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const logger = require('morgan')
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
     res.send('1861186213');
