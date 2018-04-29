79d0511a51ef418fde9f02715073fb3d3b2ed17e
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2018-04-29 22:10:58 +0400

    some changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const logger = require('morgan')
+const verification = require('./controllers/verification');
+const messageWebhook = require('./controllers/messageWebhook');
+const logger = require('morgan');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 
-app.get('/', (req, res) => {
-    res.send('1861186213');
-});
+app.get('/', verification);
+app.post('/', messageWebhook);
+
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 app.listen(port, () => console.log('Webhook server is listening, port 3000'));
