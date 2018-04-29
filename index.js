081351bfaf8145452391772dcc7f1d9577606529
Eduard-Simonyan-test <esimonyan2014@gmail.com>
2018-04-29 23:35:20 +0400

    some changes

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-//
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const verification = require('./controllers/verification');
-// const messageWebhook = require('./controllers/messageWebhook');
-// const logger = require('morgan');
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(logger('dev'));
-//
-// app.get('/webhook', verification);
-// app.post('/webhook', messageWebhook);
-//
-//
-// const port = parseInt(process.env.PORT, 10) || 30000;
-// app.listen(port, () => console.log('Webhook server is listening, port 3000'));
-
 'use strict';
 const BootBot = require('bootbot');
 
