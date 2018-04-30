9485740a333a0014ab45db4429c675b178587587
PhilStar <philstar@iMac.local>
2018-04-30 12:09:17 +0300

    ghdfg

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bot = new BootBot({
     verifyToken: process.env['FB_VERIFY_TOKEN'],
     appSecret: process.env['FB_APP_SECRET']
 });
+
 bot.on('message', (payload, chat) => {
     const text = payload.message.text;
     chat.say(`Echo f: ${text}`);
